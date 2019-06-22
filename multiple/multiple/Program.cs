51782c3ef67c4ba89e6e7a6f47b8b4fc71a31ea7
WonChang05 <50658107+WonChang05@users.noreply.github.com>
2019-06-23 01:18:44 +0800

    Add files via upload

---
+++ multiple/multiple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string flag = "y";
+            while (flag == "y")
+            {
+                //1.讓使用者輸入兩個數字，判斷第二個數字是不是第一個數字的倍數
+                Console.WriteLine("請輸入第一個數字：");
+                int a = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("請輸入第二個數字：");
+                int b = Convert.ToInt16(Console.ReadLine());
+                if ((b % a) == 0)
+                {
+                    Console.WriteLine("第二個數字是第一個數字的倍數");
+                }
+                else
+                {
+                    Console.WriteLine("第二個數字不是第一個數字的倍數");
+                }
+                Console.WriteLine("是否要繼續？y/n");
+                if (Console.ReadLine() == "n")
+                {
+                    flag = "n";
+                }
+            }
+        }
+    }
+}
